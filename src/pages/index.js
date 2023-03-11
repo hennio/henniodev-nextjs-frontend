e248f2f54dda6093de8810455d2136322ade398b
hennio <hennio@users.noreply.github.com>
2023-03-11 19:47:46 +0100

    start van develop branche.

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
             </Head>
 
             <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-                Soon..
+                Dit is de ontwikkel omgeving
             </div>
         </>
     )
